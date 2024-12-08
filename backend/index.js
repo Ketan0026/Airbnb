501f0b3a65df70a05ac71a5b76b0eaf9a775a7e4
Ketan <ketankaniz@gmail.com>
2024-12-08 16:23:40 +0530

    url changed

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "https://airbnb-silk-ten.vercel.app/",
+    origin: "https://airbnb-silk-ten.vercel.app",
     credentials: true,
   })
 );
